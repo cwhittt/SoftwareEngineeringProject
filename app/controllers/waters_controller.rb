75c0ff5a0cdbe94b1e05e79a939b702456779322
root <connor.whitt26@gmail.com>
2023-04-28 01:14:24 +0000

    user auth

--- app/controllers/waters_controller.rb
+++ app/controllers/waters_controller.rb
@@ -1,5 +1,6 @@
 class WatersController < ApplicationController
   before_action :set_water, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, only: %i[ new edit create update destroy]
 
   # GET /waters or /waters.json
   def index
