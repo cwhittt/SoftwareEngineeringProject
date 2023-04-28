678ef72d17767ea281378547611820105f919511
root <connor.whitt26@gmail.com>
2023-04-28 20:05:58 +0000

    Total Water

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    $total = 0
+    def get_Total(amount)
+        puts($total += amount)
+    end
+    def reset_Total()
+        puts($total = 0)
+    end
+    def print_Total()
+        $total
+    end
+    def print_unit(waters)
+        waters[0].unit
+    end
 end
